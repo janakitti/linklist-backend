0da95927feb085b368080b49652df432fec8b1d3
Janakitti <janakittiratana@gmail.com>
2020-12-29 15:11:15 -0500

    Initial commit

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const config = require('config');
+const jwt = require('jsonwebtoken'); 
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 15
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser(user) {
+    const schema = Joi.object({
+        username: Joi.string().min(3).max(15).required(),
+        email: Joi.string().min(5).max(255).email().required(),
+        password: Joi.string().min(5).max(255).required()
+    });
+    return schema.validate(user);
+}
+
+exports.User = User;
+exports.validate = validateUser;
