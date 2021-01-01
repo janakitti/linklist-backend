63245de35d7cdc946f2c681aaabf8bbee5979223
Janakitti <janakittiratana@gmail.com>
2020-12-31 21:10:49 -0500

    Add CORS

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
+app.use(cors());
+app.options('*', cors());
 
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
 
-const port = process.env.PORT || 3000;
+
+
+
+
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
