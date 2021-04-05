d73eb644c18a6cf1c6ed239d885345a5a4a3cd59
Janakitti <janakittiratana@gmail.com>
2021-04-05 15:49:52 -0400

    Use DB_CONNECTION env variable for mongoose.connect

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,13 @@
-const mongoose = require('mongoose');
-const config = require('config')
+const mongoose = require("mongoose");
+const config = require("config");
 
-module.exports = function() {
-    const db = config.get('db');
-    mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-        .then(() => console.log(`Connected to ${db}`));
-}
+module.exports = function () {
+  const db = config.get("db");
+  mongoose
+    .connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => console.log(`Connected to ${db}`));
+};
