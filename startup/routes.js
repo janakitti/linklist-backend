7845ec017cb10e537fbf21700ffb663319a985a0
janakitti <janakittiratana@gmail.com>
2021-12-30 16:37:48 -0500

    Use cors

--- startup/routes.js
+++ startup/routes.js
@@ -4,8 +4,10 @@ const lists = require("../routes/lists");
 const links = require("../routes/links");
 const publicLists = require("../routes/public-lists");
 const auth = require("../routes/auth");
+const cors = require("cors");
 
 module.exports = function (app) {
+  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
   app.use(express.json());
   app.use("/api/users", users);
   app.use("/api/lists", lists);
