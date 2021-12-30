fdad899ccf7c9bd3771cfc5502c8e8261a2aebd6
janakitti <janakittiratana@gmail.com>
2021-12-30 16:58:29 -0500

    Fix index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const cors = require("cors");
 const app = express();
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-//   });
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-// app.options('*', cors());
 
 require("./startup/routes")(app);
 require("./startup/db")();
