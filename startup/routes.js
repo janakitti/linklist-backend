9eb929b51e48d04bf9d52cd721ee3b50a597e2c6
janakitti <janakittiratana@gmail.com>
2021-12-30 16:46:29 -0500

    Use env for Origin

--- startup/routes.js
+++ startup/routes.js
@@ -7,7 +7,7 @@ const auth = require("../routes/auth");
 const cors = require("cors");
 
 module.exports = function (app) {
-  app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+  app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
   app.use(express.json());
   app.use("/api/users", users);
   app.use("/api/lists", lists);
