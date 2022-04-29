99fca671b459fdb15b9c1d461e7d5049edf39fc1
janakitti <janakittiratana@gmail.com>
2022-04-29 15:38:35 -0400

    Temporarily wrap nodemailer code in try/catch

--- routes/users.js
+++ routes/users.js
@@ -50,56 +50,59 @@ router.post("/", async (req, res) => {
   const token = user.generateAuthToken();
 
   const OAuth2 = google.auth.OAuth2;
+  try {
+    // Connect Linklist application to Google OAuth playground
+    const oauth2Client = new OAuth2(
+      process.env.OAUTH_CLIENTID,
+      process.env.OAUTH_CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+    );
 
-  // Connect Linklist application to Google OAuth playground
-  const oauth2Client = new OAuth2(
-    process.env.OAUTH_CLIENTID,
-    process.env.OAUTH_CLIENT_SECRET,
-    "https://developers.google.com/oauthplayground"
-  );
-
-  // Renew access token using refresh token
-  oauth2Client.setCredentials({
-    refresh_token: process.env.OAUTH_REFRESH_TOKEN,
-  });
-
-  const accessToken = await new Promise((resolve, reject) => {
-    oauth2Client.getAccessToken((err, token) => {
-      if (err) {
-        reject("Failed to create access token " + err);
-      }
-      resolve(token);
+    // Renew access token using refresh token
+    oauth2Client.setCredentials({
+      refresh_token: process.env.OAUTH_REFRESH_TOKEN,
     });
-  });
 
-  // Authenticate as owner of the Gmail account
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      type: "OAuth2",
-      user: process.env.MAIL_USERNAME,
-      accessToken,
-      clientId: process.env.OAUTH_CLIENTID,
-      clientSecret: process.env.OAUTH_CLIENT_SECRET,
-      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-    },
-  });
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject("Failed to create access token " + err);
+        }
+        resolve(token);
+      });
+    });
 
-  // Create email
-  const mailOptions = {
-    from: process.env.MAIL_USERNAME,
-    to: user.email,
-    subject: "Welcome to Linklist!",
-    html: `Please <a href="${process.env.ORIGIN}/verify?id=${verificationCode}">click here</a> to verify your email.`,
-  };
+    // Authenticate as owner of the Gmail account
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        accessToken,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      },
+    });
 
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log("Error " + err);
-    } else {
-      console.log("Confirmation email sent successfuly.");
-    }
-  });
+    // Create email
+    const mailOptions = {
+      from: process.env.MAIL_USERNAME,
+      to: user.email,
+      subject: "Welcome to Linklist!",
+      html: `Please <a href="${process.env.ORIGIN}/verify?id=${verificationCode}">click here</a> to verify your email.`,
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+      if (err) {
+        console.log("Error " + err);
+      } else {
+        console.log("Confirmation email sent successfuly.");
+      }
+    });
+  } catch (e) {
+    console.error(e);
+  }
 
   res
     .header("x-auth-token", token)
